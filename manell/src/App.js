ad409f1b07251069800387df8e856530bf859188
EishaMazhar <eisha.mazhar19@gmail.com>
2020-12-09 16:26:27 +0500

    manell store react app

---
+++ manell/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MaNell</h1>
+      <h2> A store where you'll find all the creative stuff 🖌️​🖼️​</h2>
+      {/* header */}
+      {/* home */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ manell/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAD6b7wUfDjHK0cl3Y-EHbHxrqn8xWauJ8",
+    authDomain: "manell-73a89.firebaseapp.com",
+    projectId: "manell-73a89",
+    storageBucket: "manell-73a89.appspot.com",
+    messagingSenderId: "94629869135",
+    appId: "1:94629869135:web:a725068f18e25e94b59c3a",
+    measurementId: "G-DJMLE51YRY"
+};
