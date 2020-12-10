17a27e444c646415424a1ec0f1b9292bd4a141fd
EishaMazhar <eisha.mazhar19@gmail.com>
2020-12-10 05:01:55 +0500

    detailed readme

--- manell/src/Header.js
+++ manell/src/Header.js
@@ -4,11 +4,7 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      <img
-        className="header__logo"
-        src="./static/logo.png"
-        alt="logo"
-      />
+      <img className="header__logo" src="./static/logo.png" alt="logo" />
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
