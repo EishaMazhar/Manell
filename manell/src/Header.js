b94f1e0909a3bc6f4ba1145c35e4d4b11136ff58
EishaMazhar <eisha.mazhar19@gmail.com>
2020-12-10 05:16:41 +0500

    header divs comments added

--- manell/src/Header.js
+++ manell/src/Header.js
@@ -4,12 +4,13 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
+    {/* Logo */}
       <img className="header__logo" src="./static/logo.png" alt="logo" />
-
+    {/* Search bar */}
       <div className="header__search">
         <input className="header__searchInput" type="text" />
       </div>
-
+    {/* Nav bar options */}
       <div className="header__nav">
         <div className="header__option">
           <span className="header__option1">hello, sign in</span>
